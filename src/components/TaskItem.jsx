58beb076d0364f5a39af2bc3b3dc32553a123f72
MAY367 <martinyurquina400@.com>
2025-05-01 22:55:23 -0300

    feat: Estructura base de TakItem

---
+++ src/components/TaskItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TaskItem = () => {
+  return (
+    <li>
+      <span>Texto de la tarea</span>
+      <button>X</button>
+    </li>
+  );
+};
+
+export default TaskItem;
