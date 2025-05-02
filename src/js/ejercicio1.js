5d9540d1144b9b9112595389a855f9db22d690d2
Condori Pablo M. <105952953+CondoriPabloM@users.noreply.github.com>
2025-05-01 22:06:17 -0300

    Visualización de los elementos  de la estructura  Array
    Dentro de este bloque se implementó la estructura forEach para recorrer la estructura Array y por medio de console.log, ver cada elemento (objeto) almacenado en el mismo .

--- src/js/ejercicio1.js
+++ src/js/ejercicio1.js
@@ -1,7 +1,6 @@
  const mostrarProductos = (productos) => {
-
-
-
-
-   
-  };
+      console.log("Listado de productos:");
+    productos.forEach(producto => {
+      console.log(`Producto: ${producto.descripcion} - Precio: $${producto.precio}`);
+    });
+ };
