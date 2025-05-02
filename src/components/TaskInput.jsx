5a4c373adb889b165466fc94af96a033407758d2
Huells2252 <huellsnicolasr@gmail.com>
2025-05-01 22:04:21 -0300

    feat: agregar campo input controlado para ingresar tarea

--- src/components/TaskInput.jsx
+++ src/components/TaskInput.jsx
@@ -4,6 +4,17 @@ const TaskInput = () => {
     const [texto, setTexto] = useState('');
 
     return <div>TaskInput</div>;
-};
 
+    return (
+        <div>
+          <input
+            value={texto}
+            onChange={(e) => setTexto(e.target.value)}
+            placeholder="Escribí una tarea"
+          />
+        </div>
+    );
+
+};
+  
 export default TaskInput;
