31a040232512a172fae155c78cc6815e36ede0bd
Condori Pablo M. <105952953+CondoriPabloM@users.noreply.github.com>
2025-05-01 22:17:39 -0300

    exportación de la función mostrarProductos
    Mediante la palabra reservada "export", podremos exportar nuestro archivo para que pueda ser importado por el archivo: ejercicio_adicional.js .

--- src/js/ejercicio1.js
+++ src/js/ejercicio1.js
@@ -1,4 +1,4 @@
- const mostrarProductos = (productos) => {
+export const mostrarProductos = (productos) => {
       console.log("Listado de productos:");
     productos.forEach(producto => {
       console.log(`Producto: ${producto.descripcion} - Precio: $${producto.precio}`);
