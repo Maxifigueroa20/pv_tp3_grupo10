392b78bcc120ed78e49bd9d4b538bddba8b10998
Condori Pablo M. <105952953+CondoriPabloM@users.noreply.github.com>
2025-05-02 01:04:40 -0300

    feat: mapeo de tareas
    tareas.map permitirá recorrer la estructura array. Por cada tarea (elemento de array), este generará un componente <TaskItem>. la variable "i" indicará la posición dentro del array donde nos encontremos posicionados.

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,7 +3,14 @@ import TaskItem from './TaskItem';
 const TaskList = ({ tareas, onToggle, onDelete }) => {
   return (
     <ul>
-      
+        {tareas.map((tarea, i) => (
+        <TaskItem
+          key={i}
+          tarea={tarea}
+          onToggle={() => onToggle(i)}
+          onDelete={() => onDelete(i)}
+        />
+      ))}
     </ul>
   );
 };
